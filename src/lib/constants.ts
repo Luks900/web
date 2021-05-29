d7682d45bda427012a3f8061d90a1b2c77c3a17a
Marko <goncarmarko@gmail.com>
2021-05-29 03:00:56 +0300

    Now you can install styles i guess

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,3 @@
 export const DATA_PREFIX = 'https://cdn.jsdelivr.net/gh/33kk/uso-archive@flomaster/data';
 export const FULL_PREFIX = 'https://cdn.jsdelivr.net/gh/33kk/uso-archive@full/data';
+export const USO_PREFIX = 'https://userstyles.org';
--- src/routes/style/[id].svelte
+++ src/routes/style/[id].svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import StyleScreenshotCarousel from '$lib/components/StyleScreenshotCarousel.svelte';
-	import { DATA_PREFIX } from '$lib/constants';
+	import { DATA_PREFIX, USO_PREFIX } from '$lib/constants';
 	import type { Style } from '$lib/types';
 	import { addTargetBlank,htmlToTemplate } from "$lib/utils";
 	import DOMPurify from 'dompurify';
 	import linkifyHtml from 'linkifyjs/html.js';
-	import { Col,Container,Icon,Row,Spinner } from 'sveltestrap';
+	import { Button,Col,Container,Icon,Row,Spinner } from 'sveltestrap';
 
 	let id: string, url: string, promise: Promise<Style>;
 
@@ -41,7 +41,14 @@
 		<h1 class="mb-4">{data.info.name}</h1>
 		<StyleScreenshotCarousel {data} />
 		<Row class="mt-4">
-			<Col class="col-12 order-2 order-lg-1 col-lg-7 col-xl-8">
+			<Col xs={12} class="d-flex flex-row-reverse mb-4">
+				{#if data.style.css}
+					<a href={`${DATA_PREFIX}/usercss/${data.id}.user.css`} target="_blank" class="btn btn-primary ms-2">Install</a>
+				{/if}
+				<a href={url} target="_blank" class="btn btn-outline-secondary ms-2">View JSON</a>
+				<a href={`${USO_PREFIX}/styles/${data.id}`} target="_blank" class="btn btn-outline-secondary">View on UserStyles.org</a>
+			</Col>
+			<Col xs={12} lg={7} xl={8} class="order-2 order-lg-1">
 				{#if data.info.description}
 					<h3>Description</h3>
 					<p>{@html processModalDescription(data.info.description)}</p>
@@ -54,7 +61,7 @@
 			<Col class="order-1 mb-3">
 				<h3>Details</h3>
 				<Row>
-					<Col class="col-3 col-lg-5">Rating</Col>
+					<Col xs={3} lg={5}>Rating</Col>
 					<Col>
 					{#if !data.stats.rating}
 						<Icon name="dash-circle" />
@@ -68,21 +75,21 @@
 					</Col>
 				</Row>
 				<Row>
-					<Col class="col-3 col-lg-5">Applies to</Col>
+					<Col xs={3} lg={5}>Applies to</Col>
 					<Col>
 						<a href={'/browse/styles?search='+encodeURIComponent("#"+data.info.category)}>{data.info.category}</a>
 					</Col>
 				</Row>
 				{#if data.info.author}
 					<Row>
-						<Col class="col-3 col-lg-5">Author</Col>
+						<Col xs={3} lg={5}>Author</Col>
 						<Col>
-							<a href={'/user/' + data.info.author.id}>{data.info.author.name} ({data.info.author.id})</a>
+							<a href={'/browse/styles/?search=' + encodeURIComponent("@"+data.info.author.id)}>{data.info.author.name} ({data.info.author.id})</a>
 						</Col>
 					</Row>
 				{/if}
 				<Row>
-					<Col class="col-3 col-lg-5">License</Col>
+					<Col xs={3} lg={5}>License</Col>
 					<Col>
 						{#if data.info.license !== "NO-REDISTRIBUTION"}
 							<a href={"https://spdx.org/licenses/" + data.info.license} target="_blank">{data.info.license}</a>
@@ -92,25 +99,25 @@
 					</Col>
 				</Row>
 				<Row>
-					<Col class="col-3 col-lg-5">Updated at</Col>
+					<Col xs={3} lg={5}>Updated at</Col>
 					<Col>{(new Date(data.info.updatedAt)).toLocaleString()}</Col>
 				</Row>
 				<Row>
-					<Col class="col-3 col-lg-5">Created at</Col>
+					<Col xs={3} lg={5}>Created at</Col>
 					<Col>{(new Date(data.info.createdAt)).toLocaleString()}</Col>
 				</Row>
 				<h3 class="mt-2">Stats</h3>
 				<Row>
-					<Col class="col-3 col-lg-5">Installs this week</Col>
+					<Col xs={3} lg={5}>Installs this week</Col>
 					<Col>{data.stats.installs.weekly}</Col>
 				</Row>
 				<Row>
-					<Col class="col-3 col-lg-5">Total installs</Col>
+					<Col xs={3} lg={5}>Total installs</Col>
 					<Col>{data.stats.installs.total}</Col>
 				</Row>
 			</Col>
 		</Row>
 	{:catch error}
-		{JSON.stringify(error)}
+		{error}
 	{/await}
 </Container>
