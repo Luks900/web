03859a41c8978a999745e6b78448bceea6fdb4ab
Marko <goncarmarko@gmail.com>
2021-05-29 03:01:42 +0300

    Rename package, move dependencies to devDependencies, disable SSR

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import sveltePreprocess from 'svelte-preprocess';
-//import adapter from '@sveltejs/adapter-node';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,6 +8,7 @@ const config = {
 	preprocess: sveltePreprocess(),
 
 	kit: {
+		ssr: false,
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
